f12b20cdf9d793f9209926c0d02c4496d4b8591d
kelvinkelvin2 <kelvin.meuleman@live.nl>
2021-11-23 14:26:27 +0100

    Agent movement and rotation???

--- Assets/Scripts/MLAgent.cs
+++ Assets/Scripts/MLAgent.cs
@@ -1,19 +1,181 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class MLAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rigidbody;
+    [SerializeField]
+    public float runspeed = 1.5f;
+
+    public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        MoveForward(actions.DiscreteActions);
+        Strafe(actions.DiscreteActions);
+        LookY(actions.DiscreteActions);
+        LookX(actions.DiscreteActions);
+        Jump(actions.DiscreteActions);
+
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Jump(ActionSegment<int> discreteActions)
     {
-        
+        int action = discreteActions[4];
+        if (action == 1)
+        {
+
+        }
     }
+
+    private void LookX(ActionSegment<int> discreteActions)
+    {
+        var rotateDir = Vector3.zero;
+        var action = discreteActions[2];
+
+        switch (action)
+        {
+            case 1:
+                rotateDir = transform.up * 1f;
+                break;
+            case 2:
+                rotateDir = transform.up * -1f;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 150f);
+    }
+
+    private void LookY(ActionSegment<int> discreteActions)
+    {
+        var rotateDir = Vector3.zero;
+        var action = discreteActions[3];
+
+        switch (action)
+        {
+            case 1:
+                rotateDir = transform.right * 1f;
+                break;
+            case 2:
+                rotateDir = transform.right * -1f;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 150f);
+    }
+
+
+    private void Strafe(ActionSegment<int> discreteActions)
+    {
+        Vector3 direction = transform.rotation.eulerAngles;
+        int action = discreteActions[1];
+        switch (action)
+        {
+
+            case 1:
+                direction = transform.right * 1f;
+
+                break;
+            case 2:
+                direction = transform.right * -1f;
+                break;
+
+        }
+        rigidbody.AddForce(direction * runspeed, ForceMode.VelocityChange);
+    }
+
+    private void MoveForward(ActionSegment<int> discreteActions)
+    {
+        Vector3 direction = transform.rotation.eulerAngles;
+
+        int action = discreteActions[0];
+        switch (action)
+        {
+
+            case 1:
+                direction = transform.forward * 1f;
+
+                break;
+            case 2:
+                direction = transform.forward * -1f;
+                break;
+
+        }
+
+
+        rigidbody.AddForce(direction * runspeed, ForceMode.VelocityChange);
+
+
+    }
+
+
+
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+       var a = actionsOut.DiscreteActions;
+        if (Input.GetKey(KeyCode.W)){
+            a[0] = 1;
+            print("w");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            a[0] = 2;
+            print("s");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            a[1] = 2;
+            print("a");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            a[1] = 1;
+            print("d");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            a[2] = 1;
+            print("L");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            a[2] = 2;
+            print("R");
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            a[3] = 2;
+            print("U");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            a[3] = 1;
+            print("D");
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    public override void Initialize()
+    {
+        rigidbody = FindObjectOfType<Rigidbody>();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
