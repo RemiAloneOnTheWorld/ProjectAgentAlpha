8a0bc0d1c7b9bd84b3eb38dec5cc15d0088d901a
RemiAloneOnTheWorld <470172@student.saxion.nl>
2021-11-23 10:23:36 +0100

    added setup of the unity project

---
+++ Assets/Scripts/MLAgent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+
+public class MLAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProjectAgentAlpha
 Project Agent Alpha for the minor acmgt.
+
+unity version: 2021.1.19f1
