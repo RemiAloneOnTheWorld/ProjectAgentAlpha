d37414af9171092f5898625806cd361d6a54e084
RemiAloneOnTheWorld <470172@student.saxion.nl>
2021-11-25 10:51:41 +0100

    added somesmall features

--- Assets/Scripts/MLAgent.cs
+++ Assets/Scripts/MLAgent.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
@@ -6,18 +7,24 @@ using Unity.MLAgents.Sensors;
 public class MLAgent : Agent
 {   
     [SerializeField]
+    private Material correct;
+    [SerializeField]
+    private Material glass;
+    [SerializeField]
     private Rigidbody mlAgentBody;
+    [SerializeField]
+    private GameObject backWall;
+    [SerializeField]
+    private GameObject goal;
     [SerializeField]
     public float maxSpeed = 200f;
     [SerializeField]
     private float runspeed = 1.5f;
     [SerializeField]
-    private GameObject goal;
-    [SerializeField]
     private Vector3 beginPosition;
     [SerializeField]
     private Vector3 beginrotation;
- 
+
 
     public override void Initialize()
     {
@@ -49,110 +56,78 @@ public class MLAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        MoveForward(actions.DiscreteActions);
-        Strafe(actions.DiscreteActions);
-        LookY(actions.DiscreteActions);
-        LookX(actions.DiscreteActions);
-        Jump(actions.DiscreteActions);
-        AddReward(-1f / MaxStep == 0 ? 5000 : MaxStep);
+        Move(actions.DiscreteActions);
+        Look(actions.DiscreteActions);
+        AddReward(-1f/MaxStep);
     }
 
     private void OnCollisionEnter(Collision col) {
         if(col.gameObject.CompareTag("Goal")) {
             SetReward(1f);
-            print("hit the goal");
+            StartCoroutine(GoalScored(0.5f));
             EndEpisode();
         }
         if(col.gameObject.CompareTag("Wall")) {
-            AddReward(-0.1f);
+            AddReward(-1f);
         }
     }
 
-    private void Jump(ActionSegment<int> discreteActions)
-    {
-        int action = discreteActions[4];
-        if (action == 1)
-        {
-
-        }
+    IEnumerator GoalScored(float time) {
+        backWall.GetComponent<Renderer>().material = correct;
+        yield return new WaitForSeconds(time);
+        backWall.GetComponent<Renderer>().material = glass;
     }
 
-    private void LookX(ActionSegment<int> discreteActions)
+    private void Look(ActionSegment<int> discreteActions)
     {
         var rotateDir = Vector3.zero;
-        var action = discreteActions[2];
-
-        switch (action)
-        {
-            case 1:
-                rotateDir = Vector3.up * 1f;
-                break;
-            case 2:
-                rotateDir = Vector3.up * -1f;
-                break;
-        }
-        transform.Rotate(rotateDir, Time.deltaTime * 150f);
-    }
-
-    private void LookY(ActionSegment<int> discreteActions)
-    {
-        var rotateDir = Vector3.zero;
-        var action = discreteActions[3];
+        var action = discreteActions[1];
 
         switch (action)
         {
             case 1:
                 rotateDir = Vector3.right * 1f;
                 break;
+            
             case 2:
                 rotateDir = Vector3.right * -1f;
+                break;
+            
+            case 3:
+                rotateDir = Vector3.up * 1f;
+                break;
+            
+            case 4:
+                rotateDir = Vector3.up * -1f;
                 break;
         }
         transform.Rotate(rotateDir, Time.deltaTime * 150f);
     }
 
-
-    private void Strafe(ActionSegment<int> discreteActions)
-    {
-        Vector3 direction = Vector3.zero;
-        int action = discreteActions[1];
-        switch (action)
-        {
-
-            case 1:
-                direction = transform.right * 1f;
-
-                break;
-            case 2:
-                direction = transform.right * -1f;
-                break;
-
-        }
-        mlAgentBody.AddForce(direction * runspeed, ForceMode.VelocityChange);
-    }
-
-    private void MoveForward(ActionSegment<int> discreteActions)
+    private void Move(ActionSegment<int> discreteActions)
     {
         Vector3 direction = Vector3.zero;
 
         int action = discreteActions[0];
         switch (action)
         {
-
             case 1:
                 direction = transform.forward * 1f;
-
                 break;
+
             case 2:
                 direction = transform.forward * -1f;
                 break;
+            
+            case 3:
+                direction = transform.right * -1f;
+                break;
 
+            case 4:
+                direction = transform.right * 1f;
+                break;
         }
-
-
         mlAgentBody.AddForce(direction * runspeed, ForceMode.VelocityChange);
-
-
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -163,16 +138,16 @@ public class MLAgent : Agent
         if (Input.GetKey(KeyCode.S))
             a[0] = 2;
         if (Input.GetKey(KeyCode.A))
-            a[1] = 2;
+            a[0] = 3;
         if (Input.GetKey(KeyCode.D))
-            a[1] = 1;
+            a[0] = 4;
         if (Input.GetKey(KeyCode.LeftArrow))
-            a[2] = 1;
+            a[1] = 4;
         if (Input.GetKey(KeyCode.RightArrow))
-            a[2] = 2;
+            a[1] = 3;
         if (Input.GetKey(KeyCode.UpArrow))
-            a[3] = 2;
+            a[1] = 2;
         if (Input.GetKey(KeyCode.DownArrow))
-            a[3] = 1;
+            a[1] = 1;
     }
 }
