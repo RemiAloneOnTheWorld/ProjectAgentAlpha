9f2d24be6acffd34b9e47061dec3ec37b596fb58
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2021-12-10 11:58:30 +0100

    Planet position adjustment

--- Assets/Scripts/Planet/PlanetRotation.cs
+++ Assets/Scripts/Planet/PlanetRotation.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 public class PlanetRotation : MonoBehaviour {
     [SerializeField] private float rotationSpeed;
     private Vector3 _rotationAxis;
-    
+
     private void Start() {
         _rotationAxis = Random.onUnitSphere;
     }
 
-    void Update()
-    {
+    void Update() {
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 }
