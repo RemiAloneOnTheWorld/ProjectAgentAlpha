da824bf47d16ea369f8f9b8a7d16dc6461b6b457
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2021-12-17 12:16:27 +0100

    Add gizmos to boxes

--- Assets/Scripts/Spaceship/Modules/BoxCreationModule.cs
+++ Assets/Scripts/Spaceship/Modules/BoxCreationModule.cs
@@ -1,24 +1,23 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxCreationModule : Module {
     [SerializeField] private GameObject box;
-    private List<Transform> _placementPositions = new List<Transform>();
+    [SerializeField] private Transform[] placementPositions;
 
     private void Awake() {
         EventQueue.GetEventQueue().Subscribe(EventType.PreparationPhase, GenerateBoxes);
-        for (int i = 0; i < transform.childCount; i++) {
-            if (transform.GetChild(i).CompareTag("Connection")) {
-                continue;
-            }
-            
-            _placementPositions.Add(transform.GetChild(i));
-        }
     }
     
     private void GenerateBoxes(EventData eventData) {
-        foreach (var position in _placementPositions) {
+        foreach (var position in placementPositions) {
             Instantiate(box, position.position, transform.rotation, transform);
+        }
+    }
+    
+    private void OnDrawGizmos() {
+        Gizmos.color = new Color(255, 0, 255, 0.5f);
+        foreach (var currentBox in placementPositions) {
+            Gizmos.DrawCube(currentBox.position, currentBox.localScale);   
         }
     }
 }
